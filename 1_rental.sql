bcf55bf5c9809bf0328742563fad6740ef2f64bb
Evgeniia Popovich <63310859+janesheshera@users.noreply.github.com>
2022-08-23 22:21:55 +0300

    1. Вывести всех неактивных покупателей, фильмы 2006 года и 10 последних платежей за прокат фильмов

---
+++ 1_rental.sql
@@ -0,0 +1,9 @@
+
+-- Вывести неактивных покупателей:
+select * from customer where active = 0;
+
+-- Вывести фильмы 2006 года:
+select * from film where release_year = 2006;
+
+-- Вывести 10 последних платежей:
+select * from payment order by payment_date desc limit 10;
